e2524c4223d86bc0c84483cad2159774745be367
Heta Patel <56798447+hetapatel638@users.noreply.github.com>
2025-05-21 15:42:41 +1000

    updated the code in 2 files

--- app/models.py
+++ app/models.py
@@ -132,8 +132,10 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
         return '<User {}>'.format(self.username)
 
     def set_password(self, password):
-        self.password_hash = generate_password_hash(password, method='pbkdf2:sha256')
-
+        self.password_hash = generate_password_hash(
+                                password
+                            ) 
+        
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
